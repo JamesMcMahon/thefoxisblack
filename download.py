0e5356b44daf797c36e6e100c22e6c04807ba8aa
Ory Band <oryband@gmail.com>
2014-01-05 13:38:31 +0200

    Added custom path variable; Skipping already downloaded wallpapers; Printing downloaded wallpapers + total downloaded; Changed resolutino to 1280x800; Added hashbang intro; Changed wallpapers dir to 'wallpapers';

--- download.py
+++ download.py
@@ -1,33 +1,46 @@
+#!/usr/bin/env python
 import os
+import os.path
 import requests
 
 from BeautifulSoup import BeautifulSoup as bs
 
-MAX_PAGES = 20
-SAVE_DIR = 'fox_backgrounds'
+MAX_PAGES = 1
+SAVE_DIR = 'wallpapers'
+RESOLUTION = '1280x800'
 
 def get_backgrounds():
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
     base = 'http://www.thefoxisblack.com/category/the-desktop-wallpaper-project/page/%s/'
+    c = 0
     for x in range(0, MAX_PAGES):
-        get_images_from_page(base % (x + 1))
+        c += get_images_from_page(base % (x + 1))
+
+    return c
 
 
 def get_images_from_page(url):
     html = fetchurl(url)
     soup = bs(html)
+    c = 0
     for link in soup.findAll('a'):
         href = link['href']
-        if '-1440x900' in href:
-            print 'Downloading %s' % href
+        file_name = href[href.rfind('/'):]
+        full_path = '%s%s' % (SAVE_DIR, file_name)
+        if '-%s' % RESOLUTION in href and not os.path.isfile(full_path):
+            print file_name[1:]
             r = requests.get(href)
-            with open('fox_backgrounds%s' % href[href.rfind('/'):], 'wb') as f:
+            with open(full_path, 'wb') as f:
                 f.write(r.content)
+            c += 1
+
+    return c
+
 
 def fetchurl(url):
     return requests.get(url).text
 
 
 if __name__ == '__main__':
-    get_backgrounds()
+    print '\nDownloaded %d new wallpapers.' % get_backgrounds()
