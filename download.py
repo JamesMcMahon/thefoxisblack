a81ce4b41cadd2187c301933f9cf729ab722dfc0
Ory Band <oryband@gmail.com>
2014-01-05 17:36:27 +0200

    Added option to download only new wallpapers not already downloaded; Some code re-ordering; add all .jpg files to .gitignore, not just the ones in a specific dir.

--- download.py
+++ download.py
@@ -5,41 +5,48 @@ import requests
 
 from BeautifulSoup import BeautifulSoup as bs
 
+
 MAX_PAGES = 50
 SAVE_DIR = 'wallpapers'
 RESOLUTION = '1280x800'
+STOP_IF_EXISTS = True  # Set to False to download all files even if the file exists and True to stop when it finds where it left off.
+
 
 def get_backgrounds():
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
     base = 'http://www.thefoxisblack.com/category/the-desktop-wallpaper-project/page/%s/'
+    path = os.path.abspath(SAVE_DIR)
     c = 0
     for x in range(0, MAX_PAGES):
-        c += get_images_from_page(base % (x + 1))
+        downloaded, carry_on = get_images_from_page(base % (x + 1), path)
+        c += downloaded
+        if not carry_on:
+            break;
 
     return c
 
 
-def get_images_from_page(url):
-    html = fetchurl(url)
-    soup = bs(html)
+def get_images_from_page(url, path):
+    soup = bs(requests.get(url).text)
     c = 0
     for link in soup.findAll('a'):
         href = link['href']
-        file_name = href[href.rfind('/'):]
-        full_path = '%s%s' % (SAVE_DIR, file_name)
-        if '-%s' % RESOLUTION in href and not os.path.isfile(full_path):
-            print file_name[1:]
+        wallpaper = href[href.rfind('/')+1:]
+        save_to = '%s/%s' % (path, wallpaper)
+        exists = os.path.isfile(save_to)
+
+        if STOP_IF_EXISTS and '-%s' % RESOLUTION in href and exists:
+            print "%s already downloaded, stopping." % wallpaper
+            return c, False
+        elif '-%s' % RESOLUTION in href and not exists:
+            print wallpaper
             r = requests.get(href)
-            with open(full_path, 'wb') as f:
+            with open(save_to, 'wb') as f:
                 f.write(r.content)
             c += 1
 
-    return c
-
-
-def fetchurl(url):
-    return requests.get(url).text
+    return c, True
 
 
 if __name__ == '__main__':
